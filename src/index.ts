3250193d141e06ef93c2c32e9f6b8c83ae7346d9
Emin Apturaim <eminapturaim@gmail.com>
2025-06-09 21:44:19 +0300

    feat(index):index.ts with all imports and configs

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import router from "./routes";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(router);
+
+app.get("/", (req, res) => {
+  res.send("Server is on !");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is runing on ${PORT}`);
+});
